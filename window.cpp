92603b24f4d91a8bc3b2fa4b4bb58aa2f7f7a01b
Maxim Tugunchinov <tugunchinov.ma@phystech.edu>
2022-03-20 14:47:32 +0300

    window app

---
+++ window.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+  // Initialise GLFW
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return 1;
+  }
+
+  // 4x antialiasing
+  glfwWindowHint(GLFW_SAMPLES, 4);
+
+  // OpenGL version
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Open a window and create its OpenGL context
+  GLFWwindow *window =
+      glfwCreateWindow(1024, 768, "Tutorial 01", nullptr, nullptr);
+  if (window == nullptr) {
+    std::cerr << "Failed to open GLFW window. If you have an Intel GPU, they "
+                 "are not 3.3 compatible. Try the 2.1 version of the tutorials."
+              << std::endl;
+    glfwTerminate();
+    return 2;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  // Initialize GLEW
+  if (glewInit() != GLEW_OK) {
+    std::cerr << "Failed to initialize GLEW" << std::endl;
+    glfwTerminate();
+    return 3;
+  }
+
+  // Ensure we can capture the escape key being pressed below
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+  // Dark blue background
+  glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+  do {
+    // Clear the screen.
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+  } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0);
+
+  // Close OpenGL window and terminate GLFW
+  glfwTerminate();
+
+  return 0;
+}
