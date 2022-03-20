c3e6fbf063142d41bbf0a4f48a82fbb479fc66a1
Maxim Tugunchinov <tugunchinov.ma@phystech.edu>
2022-03-20 19:17:06 +0300

    Triangle

---
+++ shader/shader.cpp
@@ -0,0 +1,97 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <GL/glew.h>
+
+#include <shader.hpp>
+
+GLuint LoadShaders(const char *vertex_file_path,
+                   const char *fragment_file_path) {
+
+  GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+  GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+  std::string VertexShaderCode;
+  std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+  if (VertexShaderStream.is_open()) {
+    std::stringstream sstr;
+    sstr << VertexShaderStream.rdbuf();
+    VertexShaderCode = sstr.str();
+    VertexShaderStream.close();
+  } else {
+    std::cerr << "Impossible to open" << vertex_file_path
+              << "Are you in the right directory?" << std::endl;
+    return 1;
+  }
+
+  std::string FragmentShaderCode;
+  std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+  if (FragmentShaderStream.is_open()) {
+    std::stringstream sstr;
+    sstr << FragmentShaderStream.rdbuf();
+    FragmentShaderCode = sstr.str();
+    FragmentShaderStream.close();
+  }
+
+  // Compile Vertex Shader
+  std::cout << "Compiling shader: " << vertex_file_path << std::endl;
+  char const *VertexSourcePointer = VertexShaderCode.c_str();
+  glShaderSource(VertexShaderID, 1, &VertexSourcePointer, nullptr);
+  glCompileShader(VertexShaderID);
+
+  // Check Vertex Shader
+  GLint Result = GL_FALSE;
+  int InfoLogLength;
+  glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+  glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+  if (InfoLogLength > 0) {
+    std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+    glGetShaderInfoLog(VertexShaderID, InfoLogLength, nullptr,
+                       &VertexShaderErrorMessage[0]);
+    std::cerr << &VertexShaderErrorMessage[0] << std::endl;
+  }
+
+  // Compile Fragment Shader
+  std::cout << "Compiling shader: " << fragment_file_path << std::endl;
+  char const *FragmentSourcePointer = FragmentShaderCode.c_str();
+  glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, nullptr);
+  glCompileShader(FragmentShaderID);
+
+  // Check Fragment Shader
+  glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+  glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+  if (InfoLogLength > 0) {
+    std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+    glGetShaderInfoLog(FragmentShaderID, InfoLogLength, nullptr,
+                       &FragmentShaderErrorMessage[0]);
+    std::cerr << &FragmentShaderErrorMessage[0] << std::endl;
+  }
+
+  // Link the program
+  std::cout << "Linking program" << std::endl;
+  GLuint ProgramID = glCreateProgram();
+  glAttachShader(ProgramID, VertexShaderID);
+  glAttachShader(ProgramID, FragmentShaderID);
+  glLinkProgram(ProgramID);
+
+  // Check the program
+  glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+  glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+  if (InfoLogLength > 0) {
+    std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
+    glGetProgramInfoLog(ProgramID, InfoLogLength, nullptr,
+                        &ProgramErrorMessage[0]);
+    std::cerr << &ProgramErrorMessage[0] << std::endl;
+  }
+
+  glDetachShader(ProgramID, VertexShaderID);
+  glDetachShader(ProgramID, FragmentShaderID);
+
+  glDeleteShader(VertexShaderID);
+  glDeleteShader(FragmentShaderID);
+
+  return ProgramID;
+}
---
+++ triangle.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include <shader.hpp>
+
+int main() {
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return 1;
+  }
+
+  glfwWindowHint(GLFW_SAMPLES, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window =
+      glfwCreateWindow(1024, 768, "Triangle", nullptr, nullptr);
+  if (window == nullptr) {
+    std::cerr << "Failed to open GLFW window. If you have an Intel GPU, they "
+                 "are not 3.3 compatible. Try the 2.1 version of the tutorials."
+              << std::endl;
+    glfwTerminate();
+    return 2;
+  }
+  glfwMakeContextCurrent(window);
+
+  glewExperimental = true;
+  if (glewInit() != GLEW_OK) {
+    std::cerr << "Failed to initialize GLEW" << std::endl;
+    glfwTerminate();
+    return 3;
+  }
+
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+  glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+  GLuint VertexArrayID;
+  glGenVertexArrays(1, &VertexArrayID);
+  glBindVertexArray(VertexArrayID);
+
+  GLuint programID = LoadShaders("SimpleVertexShader.vertexshader",
+                                 "SimpleFragmentShader.fragmentshader");
+
+  static const GLfloat g_vertex_buffer_data[] = {
+      -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+  };
+
+  GLuint vertex_buffer;
+  glGenBuffers(1, &vertex_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
+               g_vertex_buffer_data, GL_STATIC_DRAW);
+
+  do {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(programID);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glVertexAttribPointer(0,
+                          3,        // size
+                          GL_FLOAT, // type
+                          GL_FALSE, // normalized
+                          0,        // stride
+                          nullptr   // array buffer offset
+    );
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+  } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0);
+
+  glDeleteBuffers(1, &vertex_buffer);
+  glDeleteVertexArrays(1, &VertexArrayID);
+  glDeleteProgram(programID);
+
+  glfwTerminate();
+
+  return 0;
+}
